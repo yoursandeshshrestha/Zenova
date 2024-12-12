060df7cc7b23a5c2733b56a7b141b089fffe2b69
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-12-12 23:45:35 +0530

    Database: New Logic

--- Backend/src/database/database.ts
+++ Backend/src/database/database.ts
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 import { config } from "../config/app.config";
 
-const connectDatabase = async () => {
-  try {
-    await mongoose.connect(config.MONGO_URI);
-    console.log("Connected to mongoose database");
-  } catch (error) {
-    console.log("Error connecting to mongoose database");
-    process.exit(1);
+const connectDatabase = async (retries = 5, delay = 5000) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      await mongoose.connect(config.MONGO_URI);
+      console.log("Connected to MongoDB");
+      return;
+    } catch (error) {
+      console.error(
+        `Database connection failed. Retrying in ${delay}ms...`,
+        error
+      );
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
   }
+  console.error("Could not connect to MongoDB. Exiting.");
+  process.exit(1);
 };
 
 export default connectDatabase;
