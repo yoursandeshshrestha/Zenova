c545af6fd2edf419b0304101158920da256fc052
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-12-12 21:50:11 +0530

    ErrorHandler: Syntax Error

--- Backend/src/middleware/errorHandler.ts
+++ Backend/src/middleware/errorHandler.ts
@@ -8,6 +8,12 @@ export const errorHandler: ErrorRequestHandler = (
 ): any => {
   console.log(`Error occured PATH: ${req.path}`, error);
 
+  if (error instanceof SyntaxError) {
+    return res.status(400).json({
+      message: "Invaild JSON format, please check your request body",
+    });
+  }
+
   return res.status(500).json({
     message: "Internal Server Error",
     error: error?.message || "Unknown error occurred",
